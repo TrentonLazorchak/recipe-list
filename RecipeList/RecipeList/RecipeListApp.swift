fa9edf48cdb9d16c4130d91caecc9b0a127b5919
Trenton Lazorchak <>
2025-05-30 18:06:44 -0400

    Create Xcode project.

---
+++ RecipeList/RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Trenton Lazorchak on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeList/RecipeListTests/RecipeListTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeListTests.swift
+//  RecipeListTests
+//
+//  Created by Trenton Lazorchak on 5/30/25.
+//
+
+import Testing
+
+struct RecipeListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
